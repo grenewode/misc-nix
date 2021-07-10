9b17f1504b6808a64727c60d4c4b2ea80dc22782
grenewode <grenewode@users.noreply.github.com>
2021-07-10 11:08:26 -0400

    Update to use just system packages

--- default.nix
+++ default.nix
@@ -1,12 +1,5 @@
-{ nixpkgs ? <nixpkgs>
-, nixpkgs-unstable ? (builtins.fetchTarball "https://releases.nixos.org/nixpkgs/nixpkgs-21.05pre277683.4e0d3868c67/nixexprs.tar.xz")
-, system ? builtins.currentSystem
-}:
-let
-  pkgs = import nixpkgs { inherit system; };
-  unstable-pkgs = import nixpkgs-unstable { inherit system; };
-in
+{ pkgs ? import <nixpkgs> {} }:
 {
-  neovim = unstable-pkgs.callPackage ./neovim { };
+  neovim = pkgs.callPackage ./neovim { };
   proton-bridge-cli = pkgs.callPackage ./proton-bridge/cli.nix { };
 }
